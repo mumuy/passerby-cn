45ee06a9e8fd5c5a0100729623ebeffeb52b360d
Maxiaoxiang <251445460@qq.com>
2015-11-20 00:05:23 +0800

    updated
    新增food

--- data/food.js
+++ data/food.js
@@ -16,7 +16,48 @@ var _data = [
         'url':'http://www.meishichina.com/'     
     },
     {
-        'name':'美食博客',
-        'url':'http://blog.sina.com.cn/lm/eatblog.html'     
+        'name':'中国菜谱网',
+        'url':'http://www.chinacaipu.com/'     
+    },
+    {
+        'name':'美食吧',
+        'url':'http://www.sbar.com.cn/'     
+    },
+    {
+        'name':'菜肴之家',
+        'url':'http://www.haozuo123.com/'     
+    },
+    {
+        'name':'天天饮食',
+        'url':'http://cctv.cntv.cn/lm/tiantianyinshi/'     
+    },
+    {
+        'name':'美食圈',
+        'url':'http://www.bilibili.com/video/ent-food-1.html'     
+    },
+    {
+        'name':'cookpad',
+        'url':'http://cookpad.com/'     
+    },
+    {
+        'name':'手工客',
+        'url':'http://www.shougongke.com/category/13.html'     
+    },
+    {
+        'name':'寻味网',
+        'url':'http://www.xunweichina.com/'     
+    },
+    {
+        'name':'101cookbooks',
+        'url':'http://www.101cookbooks.com/'     
+    },
+    {
+        'name':'seriouseats',
+        'url':'http://www.seriouseats.com/'     
+    },
+    {
+        'name':'foodgawker',
+        'url':'https://foodgawker.com/'     
     }
+
 ];
