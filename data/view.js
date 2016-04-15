16b61ccc53f171d8ece5eed4d6e070e9ecd57d4c
郑浩乐 <89932980@qq.com>
2016-04-15 09:40:54 +0800

    update 添加阅读站点

--- data/view.js
+++ data/view.js
@@ -2,5 +2,17 @@ var _data = [
 	{
 		'name':'界面新闻',
 		'url':'http://www.jiemian.com/'
+	},
+	{
+		'name':'左岸读书',
+		'url':'http://www.zreading.cn/'
+	},
+	{
+		'name':'简书',
+		'url':'http://www.jianshu.com/'
+	},
+	{
+		'name':'译言',
+		'url':'http://www.yeeyan.org/'
 	}
 }
